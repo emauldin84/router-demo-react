8ab21ae3ea043bc318b97e61d63f73ca788495df
Eric Mauldin <emauldin84@gmail.com>
2019-04-30 15:20:57 -0400

    builds initial skeleton with links to home, about and koa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+import Home from './Home';
+import About from './About';
+import Koa from './Koa';
+
+import { Link, Route, } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to="/">Home </Link>
+      <Link to="/about">About </Link>
+      <Link to="/koa">Koa </Link>
+      
+      {/* <Home /> */}
+      <Route exact path="/" component={Home} />
+      {/* <About /> */}
+      <Route path="/about" component={About} />
+      {/* <Koa /> */}
+      <Route path="/koa" component={Koa} />
+
     </div>
   );
 }
---
+++ src/Koa.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Koa(props) {
+    return (
+        <div>
+            <h1>Koa</h1>
+        </div>
+    )
+}
+
+
+export default Koa;
